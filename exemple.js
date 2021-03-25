52df9ba4650aa371791b0e1494b2a7ac1ba735ce
Yeun <yeun.lefloch@gmail.com>
2021-03-25 09:18:47 +0100

    Commit de l'app météo avec géolocalisation

---
+++ exemple.js
@@ -0,0 +1,77 @@
+let villeChoisie;
+
+if('geolocation' in navigator){
+    navigator.geolocation.watchPosition((position)=>{
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' +position.coords.longitude+ '&lat=' + position.coords.latitude + '&appid=a2a42c65541f8ad5b5de1f5268cdb342&units=metric';
+        console.log(url);
+  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+  requete.open('GET', url); // Nous récupérons juste des données
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dès qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response;
+        // console.log(reponse);
+        let temperature = reponse.main.temp;
+        let ville       = reponse.name;
+        // console.log(temperature);
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector('#ville').textContent = ville;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
+    }
+  }
+    },error,options);
+}
+else{
+    villeChoisie='Paris';
+    recevoirTemperature(villeChoisie);
+}
+
+var options = {
+    enableHighAccuracy:true
+}
+
+
+let changerDeVille = document.querySelector('#changer');
+changerDeVille.addEventListener('click', () => {
+  villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
+  recevoirTemperature(villeChoisie);
+});
+
+function error(){
+    villeChoisie="Paris";
+    recevoirTemperature(villeChoisie);
+}
+
+function recevoirTemperature(ville) {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=a2a42c65541f8ad5b5de1f5268cdb342&units=metric';
+
+  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+  requete.open('GET', url); // Nous récupérons juste des données
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dès qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response;
+        // console.log(reponse);
+        let temperature = reponse.main.temp;
+        let ville       = reponse.name;
+        // console.log(temperature);
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector('#ville').textContent = ville;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
+    }
+  }
+}
